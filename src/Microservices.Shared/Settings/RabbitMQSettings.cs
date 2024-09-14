ea1bbc1c2c92cecf0e5244e2e0319a539296bc0f
Angelo Pirola <angelo9871@gmail.com>
2024-09-14 09:58:09 +0200

    Aggiunta configurazione Settings

---
+++ src/Microservices.Shared/Settings/RabbitMQSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Microservices.Shared.Settings;
+
+public class RabbitMQSettings
+{
+    public string Hostname { get; set; } = null!;
+    public string Username { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string QueueReceiver { get; set; } = null!;
+}
---
+++ src/Microservices.Shared/Settings/VaultSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Microservices.Shared.Settings;
+
+public class VaultSettings
+{
+    public string Address { get; set; } = null!;
+    public string Token { get; set; } = null!;
+    public string MountPath { get; set; } = null!;
+    public string SecretKey { get; set; } = null!;
+}
