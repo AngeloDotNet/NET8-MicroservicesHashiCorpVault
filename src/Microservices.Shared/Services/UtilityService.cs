6e7a8492e28ce1e84dc0cb4f74a49843ac210697
Angelo Pirola <angelo9871@gmail.com>
2024-09-14 09:58:23 +0200

    Aggiunta configurazione Services e D. I.

---
+++ src/Microservices.Shared/Services/UtilityService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microservices.Shared.Services;
+
+public static class UtilityService
+{
+    public static T? ConfigureAndGet<T>(this IServiceCollection services, IConfiguration configuration, string sectionName) where T : class
+    {
+        var section = configuration.GetSection(sectionName);
+        var settings = section.Get<T>();
+        services.Configure<T>(section);
+
+        return settings;
+    }
+}
