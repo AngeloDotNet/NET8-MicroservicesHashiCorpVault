a828b1f6cf711461f4b8d2344bff1e9fff76f390
Angelo Pirola <angelo9871@gmail.com>
2024-09-16 15:32:11 +0200

    Aggiunto DBContext e relative entità

---
+++ src/Microservices.Products/DataAccessLayer/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microservices.Products.DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microservices.Products.DataAccessLayer;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    { }
+
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.ToTable("Products");
+            entity.HasKey(entity => entity.Id);
+        });
+    }
+}
