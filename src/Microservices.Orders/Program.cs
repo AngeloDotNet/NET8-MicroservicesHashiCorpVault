5d8750a641dab4e0423585ac23851367af49ad71
Angelo Pirola <angelo9871@gmail.com>
2024-09-19 10:23:28 +0200

    Aggiunto progetto Ordini

---
+++ src/Microservices.Orders/Program.cs
@@ -0,0 +1,49 @@
+using Microservices.Orders.Consumer;
+using Microservices.Orders.DataAccessLayer;
+using Microservices.Shared.Services;
+using Microservices.Shared.Settings;
+using Microsoft.EntityFrameworkCore;
+using Vault;
+using Vault.Client;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+var vaultSettings = builder.Services.ConfigureAndGet<VaultSettings>(builder.Configuration, nameof(VaultSettings));
+var rabbitMQSettings = builder.Services.ConfigureAndGet<RabbitMQSettings>(builder.Configuration, nameof(RabbitMQSettings));
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton((serviceProvider) =>
+{
+    var config = new VaultConfiguration(vaultSettings!.Address);
+    var vaultClient = new VaultClient(config);
+
+    vaultClient.SetToken(vaultSettings!.Token);
+
+    return vaultClient;
+});
+
+var connectionString = DependencyInjection.GetConnectionDatabase(vaultSettings!);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(connectionString, options =>
+    {
+        options.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName);
+    });
+});
+
+builder.Services.AddConsumerRabbitMQ<ProductCreatedConsumer>(rabbitMQSettings!);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+app.Run();
