043efbe288e958195950fa0e34271576d6c18238
Angelo Pirola <angelo9871@gmail.com>
2024-11-04 01:10:42 +0100

    Refactoring SHARED

--- src/Microservices.Shared/Models/DatabaseDTO.cs
+++ src/Microservices.Shared/Models/DatabaseDTO.cs
@@ -1,9 +1,3 @@
 ﻿namespace Microservices.Shared.Models;
 
-public class DatabaseDTO
-{
-    public string DbHostname { get; set; } = null!;
-    public string DbDatabase { get; set; } = null!;
-    public string DbUsername { get; set; } = null!;
-    public string DbPassword { get; set; } = null!;
-}
+public record class DatabaseDTO(string SqlConnection);
---
+++ src/Microservices.Shared/Vault/VaultSecretProvider.cs
@@ -0,0 +1,41 @@
+﻿using VaultSharp;
+using VaultSharp.V1.AuthMethods.Token;
+
+namespace Microservices.Shared.Vault;
+
+public class VaultSecretProvider
+{
+    private readonly IVaultClient vaultClient;
+
+    public VaultSecretProvider(string vaultUri, string vaultToken)
+    {
+        var authMethod = new TokenAuthMethodInfo(vaultToken);
+        var vaultClientSettings = new VaultClientSettings(vaultUri, authMethod);
+
+        vaultClient = new VaultClient(vaultClientSettings);
+    }
+
+    public async Task<string> GetSecretValueAsync(string path, string key, string mountPoint)
+    {
+        var secret = await vaultClient.V1.Secrets.KeyValue.V2.ReadSecretAsync(path: path, mountPoint: mountPoint);
+
+        if (secret.Data.Data.TryGetValue(key, out var value))
+        {
+            return value.ToString()!;
+        }
+
+        throw new KeyNotFoundException($"Key '{key}' not found in Vault at path '{path}'");
+    }
+
+    public async Task<bool> SetSecretValueAsync(string path, string key, string value, string mountPoint)
+    {
+        var secret = new Dictionary<string, object>
+        {
+            { key, value }
+        };
+
+        var writeSecret = await vaultClient.V1.Secrets.KeyValue.V2.WriteSecretAsync(path: path, secret, checkAndSet: null, mountPoint: mountPoint);
+
+        return true;
+    }
+}
